ca710f21db215eb8c4d470da27ff8bb1e4a5a9ae
Kenton Hamaluik <kenton@hamaluik.ca>
2018-09-02 16:24:36 -0600

    made notelist take from store

--- src/ui/components/NoteList.hx
+++ src/ui/components/NoteList.hx
@@ -1,26 +1,32 @@
 package ui.components;
 
+import data.types.Note;
 import mithril.M;
 
 class NoteList implements Mithril {
     public static function view(node:Vnode<NoteList>): Vnodes {
-        var id:String = node.attrs.get('id');
-
-        // TODO: fetch notes from store
+        var id:Null<String> = node.attrs.get('id');
+        var notes:Array<Note> = App.store.getState().notes.getAll();
         // TODO: apply filter
+        notes.sort(function(a:Note, b:Note):Int {
+            // TODO: use the sorting buttons
+            return Std.int(a.lastModified.getTime() - b.lastModified.getTime());
+        });
+
+        var no_notes_notice:Vnodes = if(notes.length == 0) {
+            m('p.content.has-text-centered', 'No notes yet!');
+        }
+        else null;
+
         return 
-        m('section.list', [
+        m('section.list', [for(note in notes)
             m(NoteInList, {
-                title: "Mom's Chilli",
-                date: "Tue Aug 28, 22:47",
-                tags: ["Recipe"],
-                selected: true
-            }),
-            m(NoteInList, {
-                title: "Beef Stew",
-                date: "Tue Aug 28, 22:52",
-                tags: ["Recipe", "Instant Pot"]
-            }),
-        ]);
+                id: note.id,
+                title: note.title,
+                date: note.lastModified.toString(),
+                tags: note.tags,
+                selected: id != null && note.id == id
+            })
+        ], no_notes_notice);
     }
 }
